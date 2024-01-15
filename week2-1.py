7925c3bd63145a080f20fdd93769edefc5d65548
Juwon Moon <102275453+Juwon-Moon@users.noreply.github.com>
2024-01-15 15:59:51 +0900

    Create week2-1.py

---
+++ week2-1.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        cnt = Counter(arr)
+        return len(set(cnt.values())) == len(cnt.values())
