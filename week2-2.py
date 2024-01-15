257930fdff06de45b2eb0bb67e8e9cf3f2f4882c
Juwon Moon <102275453+Juwon-Moon@users.noreply.github.com>
2024-01-15 16:00:45 +0900

    Create week2-2.py

---
+++ week2-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+  
+        tmp = []
+        for string in s:
+            ans = ""
+            if string == ']':
+                while tmp[-1] != '[':
+                    ans += tmp.pop()
+                tmp.pop()
+                c = []
+                while len(tmp) > 0:
+                    if tmp[-1].isdigit():
+                        c.append(int(tmp.pop()))
+                    else: break
+                test = sum([x*(10**i) for i,x in enumerate(c)])
+                tmp.extend(list(ans[::-1]) * sum([x*(10**i) for i,x in enumerate(c)]))
+            else:
+                tmp.append(string)
+            
+        return "".join(tmp)
